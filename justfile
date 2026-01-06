4303e4f887f0d2f69360f19015c1479ad5f96215
Thibaut <thibaut2saivre@gmail.com>
2026-01-06 22:49:35 +0100

    add slint scaffold

---
+++ justfile
@@ -0,0 +1,3 @@
+# Run slint live preview (.slint files are live-reloaded)
+live-preview:
+  SLINT_LIVE_PREVIEW=1 cargo run --features slint/live-preview
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
 use clap::Parser;
 
 use crate::cli::Args;
 
 mod cli;
 
-fn main() {
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     if args.refresh {
         println!("Refreshing application cache...");
         // Call the function to refresh the cache here
     } else {
-        println!("Running app launcher...");
-        // Call the function to run the app launcher here
+        let ui = Launcher::new()?;
+        ui.run()?;
     }
+
+    Ok(())
 }
