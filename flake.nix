28b77d9ad5437e43d80f05f6b6cbcc1ec559c5c3
Thibaut <thibaut2saivre@gmail.com>
2026-01-07 22:08:44 +0100

    add flake package entry

--- flake.nix
+++ flake.nix
@@ -41,5 +41,37 @@
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
         }
       );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          minilauncher = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "minilauncher";
+            version = "0.1.0";
+
+            src = ./.;
+            cargoHash = "sha256-CCyoBXi28Ko5AxU89Sfy3FUpAhwIVs/kDSdaXJmzYPE=";
+
+            buildInputs = with pkgs; [
+              libGL
+              libxkbcommon
+              wayland
+              fontconfig
+            ];
+
+            postFixup = ''
+              patchelf --set-rpath "${pkgs.lib.makeLibraryPath buildInputs}" $out/bin/minilauncher
+            '';
+
+            meta = {
+              description = "Fancy and minimal app launcher build with Slint";
+              homepage = "https://github.com/GnRlLeclerc/MiniLauncher";
+            };
+          };
+        }
+      );
     };
 }
